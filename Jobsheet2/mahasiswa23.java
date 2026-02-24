c53047fe5234ddbae381ef5bb7c2515ee096c37b
borzooraa <nurfakiyahr24@gmail.com>
2026-02-24 16:52:19 +0700

    Percobaan 1, tidak memakai main class

---
+++ Jobsheet2/mahasiswa23.java
@@ -0,0 +1,35 @@
+public class mahasiswa23 {
+   String nama, nim, kelas;
+    double ipk;
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + nim);
+        System.out.println("Kelas: " + kelas);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIPK(double ipkBaru) {
+        if (ipk > 4.0 || ipk < 0.0) {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        } else{
+            ipk = ipkBaru;
+        }
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    } 
+}
