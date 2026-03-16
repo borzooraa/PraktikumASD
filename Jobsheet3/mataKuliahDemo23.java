8e5722d9d4db7bb01e2d5fe1da365c4499eaad2b
borzooraa <nurfakiyahr24@gmail.com>
2026-03-16 11:33:44 +0700

    percobaan 3

---
+++ Jobsheet3/mataKuliahDemo23.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class mataKuliahDemo23 {
+    public static void main(String[] args) {
+        Scanner kiyah = new Scanner(System.in);
+        mata_Kuliah23[] arrayOfMataKuliah23 = new mata_Kuliah23[3];
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        for (int i = 0; i < arrayOfMataKuliah23.length; i++) {
+            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
+            System.out.print("Kode        : ");
+            kode = kiyah.nextLine();
+            System.out.print("Nama        : ");
+            nama = kiyah.nextLine();
+            System.out.print("Sks         : ");
+            dummy = kiyah.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah jam  : ");
+            dummy = kiyah.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("---------------------------------------");
+
+            arrayOfMataKuliah23[i] = new mata_Kuliah23(kode, nama, sks, jumlahJam);
+        }
+        for (int i = 0; i < arrayOfMataKuliah23.length; i++) {
+            System.out.println("Data Matakuliah ke-" + (i + 1));
+            System.out.println("Kode          : " + arrayOfMataKuliah23[i].kode);
+            System.out.println("Nama          : " + arrayOfMataKuliah23[i].nama);
+            System.out.println("Sks           : " + arrayOfMataKuliah23[i].sks);
+            System.out.println("Jumlah jam    : " + arrayOfMataKuliah23[i].jumlahJam);
+            System.out.println("---------------------------------------");
+        }
+    }
+}
---
+++ Jobsheet3/mata_Kuliah23.java
@@ -0,0 +1,11 @@
+public class mata_Kuliah23 {
+    public String kode, nama;
+    public int sks, jumlahJam;
+
+    public mata_Kuliah23(String kode, String nama, int sks, int jumlahJam){
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
