6bf20ca8f99787c157592081e24001080edb527c
borzooraa <nurfakiyahr24@gmail.com>
2026-03-18 12:43:02 +0700

    Percobaan1_Pertanyaan2

--- jobsheet5/faktorial23.java
+++ jobsheet5/faktorial23.java
@@ -3,8 +3,10 @@ package jobsheet5;
 public class faktorial23 {
     int faktorialBF(int n){
         int fakto =1;
-        for (int i = 1; i <=n; i++) {
+        int i=1;
+        while (i<=n){
             fakto = fakto * i;
+            i++;
         }
         return fakto;
     }
